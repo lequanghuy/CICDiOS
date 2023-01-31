87fc84ecd0bc8141402a99951c174d2bd90251ea
Lê Quang Huy <lequanghuy16195@gmail.com>
2023-01-31 14:50:36 +0700

    commit feature 1

--- retiment-calculator/ContentView.swift
+++ retiment-calculator/ContentView.swift
@@ -14,6 +14,12 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button {
+                print("Tap")
+            } label: {
+                Text("Tap me")
+            }
+
         }
         .padding()
     }
