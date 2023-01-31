48b55a6f95404578dd113ba0ea4196cf9a14584b
Lê Quang Huy <lequanghuy16195@gmail.com>
2023-01-31 08:57:03 +0700

    Initial Commit

---
+++ retiment-calculator/retiment_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  retiment_calculatorApp.swift
+//  retiment-calculator
+//
+//  Created by Huy Lê on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct retiment_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ retiment-calculatorUITests/retiment_calculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  retiment_calculatorUITestsLaunchTests.swift
+//  retiment-calculatorUITests
+//
+//  Created by Huy Lê on 31/01/2023.
+//
+
+import XCTest
+
+final class retiment_calculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
